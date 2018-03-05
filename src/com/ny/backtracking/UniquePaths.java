dd91071132bede3a6fc7078a6db6276b7e0db8ab
niye96 <526864551@qq.com>
2018-03-05 17:37:26 +0800

    ***经典问题，从一个角走到斜对角，dp或者数学解法

---
+++ src/com/ny/backtracking/UniquePaths.java
@@ -0,0 +1,33 @@
+package com.ny.backtracking;
+
+/**
+ * @Author: ny
+ * @Date: Created in 17:20 2018/3/5 0005
+ */
+public class UniquePaths {
+    public static int uniquePaths(int m, int n) {
+//        int[][] matrix = new int[m][n];
+//        for(int i = 0; i < n;i++)matrix[0][i]=1;
+//        for(int i = 0; i < m; i++)matrix[i][0]=1;
+//        for(int i = 1; i < m;i++){
+//            for(int j = 1; j < n;j++){
+//                matrix[i][j]=matrix[i-1][j]+matrix[i][j-1];
+//            }
+//        }
+//        return matrix[m - 1][n -1];
+        long total = m + n - 1 - 1;
+        long min = Math.min(m - 1, n - 1);
+        long res = 1;
+        for (int i = 1; i <= min; i++) {
+            res *= (total + 1 - i);
+            res /= i;
+        }
+        return (int) res;
+    }
+
+    public static void main(String[] args) {
+        uniquePaths(51, 9);
+        uniquePaths(1, 2);
+        uniquePaths(1, 1);
+    }
+}
